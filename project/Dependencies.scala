6ad7947ac4dd578339c2541e6cb66cb6e5b023ce
a-fatah <abdulfatah66@gmail.com>
2023-02-23 04:51:53 +0100

    add missing dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
   lazy val munitScalacheck = "org.scalameta" %% "munit-scalacheck" % "0.7.29"
   lazy val slick = "com.typesafe.slick" %% "slick" % "3.3.3"
+  lazy val slickHikaricp = "com.typesafe.slick" %% "slick-hikaricp" % "3.3.3"
   lazy val postgresql = "org.postgresql" % "postgresql" % "42.2.23"
-  lazy val hikaricp = "com.zaxxer" % "HikariCP" % "4.0.3"
   lazy val h2 = "com.h2database" % "h2" % "1.4.200"
 }
